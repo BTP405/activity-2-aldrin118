94e775064552bd965612bca3d78c62185986bed6
aldrin118 <158625299+aldrin118@users.noreply.github.com>
2024-02-12 13:13:25 +0000

    q1 and q2 copy

--- q1.py
+++ q1.py
@@ -1,1 +1,16 @@
 def getPrimeNumbers(n):
+    primeList = []
+
+    #loops cannot be avoided since each between 2 and n needs to be check if its a prime number or not.
+    for num in range(2,n): #loops through 2 to n
+        prime = True    #set the current num to isPrime
+        #no code is repeated
+        for i in range(2,num): #loops num of its divisible by any number between 2 and num
+            if (num%i==0):  #check if divisible by any number between 2 and num
+                prime = False #if divisible not a prime
+        if prime:
+            primeList.append(num) #if not divisible append to the prime list.
+
+    return primeList
+
+print(getPrimeNumbers(101))
--- q2.py
+++ q2.py
@@ -1,1 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def graphSnowfall(t):
+    f = open(t, 'r') #opens the file
+    data = [] #create the list of data
+    for line in f:  #loops and read the data from the file on each line
+        if(line != '\n'):
+            data.append(int(line))
+
+    graphData = [0,0,0,0,0]
+    yaxis=[1,2,3,4,5]
+    for num in data:
+        if(num <= 10):
+            graphData[0]+=1
+        elif(num > 10 and num <= 20):
+            graphData[1]+=1
+        elif(num >20 and num <=30):
+            graphData[2]+=1
+        elif(num >30 and num <=40):
+            graphData[3]+=1
+        elif(num >40 and num <=50):
+            graphData[4]+=1
+
+    fig, ax = plt.subplots()
+    ax.plot(graphData, yaxis)
+    ax.set(xlabel='Level', ylabel='Number', title='Snow Levels')
+    ax.grid()
+    fig.savefig("test.png")
+    plt.show()
+
+    print(graphData)
+    
+
+
+graphSnowfall("file.txt")
